0a16aafc7fd47f64602b5f570ff61f1e11aea67e
Joel  Bly <jtb6179@g.rit.edu>
2019-11-26 04:48:40 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,54 @@
+def using_push(array, string)
+ array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+ array.pop
+ end
+ 
+ def pop_with_args(array)
+   array.pop(2)
+ end
+ 
+ def using_shift(array)
+   array.shift
+ end
+ 
+ def shift_with_args(random_array)
+   random_array.shift(2)
+ end
+ 
+ def using_concat(favs, strings)
+   favs.concat(strings)
+ end
+ 
+ def using_insert(list, string)
+   list.insert(4, string)
+ end
+ 
+ def using_uniq(haircuts) 
+   haircuts.uniq 
+ end
+ 
+ def using_flatten(instruments)
+   instruments.flatten
+ end
+ 
+ def using_delete(instructors, text)
+   instructors.delete(text)
+ end
+ 
+ def using_delete_at(robos, num)
+   robos.delete_at(2)
+ end
+ 
+   
+ 
+ 
+   
+ 
+ 
